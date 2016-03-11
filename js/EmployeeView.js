b312f2808796a14e0990db1aa6e8a22f37d805c1
Avais Khatri <avaiskhatri@hotmail.com>
2016-03-11 23:16:21 +0500

    update

--- js/EmployeeView.js
+++ js/EmployeeView.js
@@ -9,17 +9,34 @@ var EmployeeView = function(employee) {
 		navigator.geolocation.getCurrentPosition(
 			function(position) {
 				$('.location', this.el).html(position.coords.latitude + ',' + position.coords.longitude);
-				new app.showAlert('Location Fetch', 'Info');
+				console.log('Location Fetch');
 			},
 			function() {
-				new app.showAlert('Unable to get location', 'Error');
+				app.showAlert('Unable to get location', 'Error');
 				//alert('Error getting location');
 			});
+		return false;
+	};
+	this.addToContacts = function(event) {
+		event.preventDefault();
+		console.log('addToContacts');
+		if (!navigator.contacts) {
+			app.showAlert("Contacts API not supported", "Error");
+			return;
+		}
+		var contact = navigator.contacts.create();
+		contact.name = {givenName: employee.firstName, familyName: employee.lastName};
+		var phoneNumbers = [];
+		phoneNumbers[0] = new ContactField('work', employee.officePhone, false);
+		phoneNumbers[1] = new ContactField('mobile', employee.cellPhone, true); // preferred number
+		contact.phoneNumbers = phoneNumbers;
+		contact.save();
 		return false;
 	};
 	this.initialize = function() {
         this.el = $('<div/>');
 		this.el.on('click', '.add-location-btn', this.addLocation);
+		this.el.on('click', '.add-contact-btn', this.addToContacts);
     };
  
     this.initialize();
