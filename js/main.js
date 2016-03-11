b7386aab36dd10f3256c5be2c324ec1aaf9e6f30
Avais Khatri <avaiskhatri@hotmail.com>
2016-03-11 11:40:41 +0500

    update

--- js/main.js
+++ js/main.js
@@ -1,17 +1,4 @@
 var app = {
-
-    findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
-    },
 	showAlert: function (message, title) {
 		if (navigator.notification) {
 			navigator.notification.alert(message, null, title, 'OK');
@@ -19,13 +6,32 @@ var app = {
 			alert(title ? (title + ": " + message) : message);
 		}
 	},	
-
+	renderHomeView: function() {
+		/*var html =
+				"<div class='header'><h1>PG Build App</h1></div>" +
+				"<div class='search-view'>" +
+				"<input class='search-key'/>" +
+				"<ul class='employee-list'></ul>" +
+				"</div>"
+		$('body').html(html);
+		$('.search-key').on('keyup', $.proxy(this.findByName, this));*/
+		$('body').html(this.homeTpl());
+		$('.search-key').on('keyup', $.proxy(this.findByName, this));
+	},
+    findByName: function() {
+		var self = this;
+		this.store.findByName($('.search-key').val(), function(employees) {
+			$('.employee-list').html(self.employeeLiTpl(employees));
+		});
+    },
     initialize: function() {
         var self = this;
 		this.store = new MemoryStore(function() {
 			self.showAlert('Store Initialized', 'Info');
+			self.renderHomeView();
 		});
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+		this.homeTpl = Handlebars.compile($("#home-tpl").html());
+		this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
     }
 
 };
