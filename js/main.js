f61a954c0208c24c742153cb2c3e1e84d8cafd68
Avais Khatri <avaiskhatri@hotmail.com>
2016-03-11 12:20:29 +0500

    update

--- js/main.js
+++ js/main.js
@@ -6,32 +6,14 @@ var app = {
 			alert(title ? (title + ": " + message) : message);
 		}
 	},	
-	renderHomeView: function() {
-		/*var html =
-				"<div class='header'><h1>PG Build App</h1></div>" +
-				"<div class='search-view'>" +
-				"<input class='search-key'/>" +
-				"<ul class='employee-list'></ul>" +
-				"</div>"
-		$('body').html(html);
-		$('.search-key').on('keyup', $.proxy(this.findByName, this));*/
-		$('body').html(this.homeTpl());
-		$('.search-key').on('keyup', $.proxy(this.findByName, this));
-	},
-    findByName: function() {
-		var self = this;
-		this.store.findByName($('.search-key').val(), function(employees) {
-			$('.employee-list').html(self.employeeLiTpl(employees));
-		});
-    },
+	
+    
     initialize: function() {
         var self = this;
 		this.store = new MemoryStore(function() {
 			self.showAlert('Store Initialized', 'Info');
-			self.renderHomeView();
+			$('body').html(new HomeView(self.store).render().el);
 		});
-		this.homeTpl = Handlebars.compile($("#home-tpl").html());
-		this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
     }
 
 };
